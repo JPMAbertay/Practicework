a29bdfec8678563739235beb159f383888b1f8d7
Jenson <2405923@uad.ac.uk>
2025-05-06 13:44:18 +0100

    commenting

--- Booking System/bookings.cpp
+++ Booking System/bookings.cpp
@@ -1,10 +1,12 @@
 #include "bookings.h"
 
-Bookings::Bookings() {
+Bookings::Bookings() {}
+	// Constructor}
+Bookings::~Bookings() {}
+	// destructor
 
-}
 
-void Bookings::setRoomType(std::string type_)
+/*void Bookings::setRoomType(std::string type_)
 {
 	roomType = type_;
 }
@@ -17,15 +19,15 @@ void Bookings::setRoomNumber(int roomNum_)
 void Bookings::setDate(int date_)
 {
 	date = date_;
-}
+} */
 
 void Bookings::createBooking(std::string type_, int roomNum_, int date_) {
-	for (int i = 0; i < BOOKINGSIZE; i++) {
+	for (int i = 0; i < BOOKINGSIZE; i++) { // create as many bookings as booking size (constant value)
 		auto booking = new Bookings();
 		booking->setRoomType(type_);
 		booking->setRoomNumber(roomNum_);
-		booking->setDate(date_);
-		bookings.push_back(booking);
+		booking->setDate(date_);// set the varaibles for the new object
+		bookings.push_back(booking); // then push it back into the vector
 
 	}
 }
@@ -34,8 +36,8 @@ std::string Bookings::listBookings() {
 	for (int i = 0; i < bookings.size(); i++) {
 		std::string type_ = bookings[i]->roomType; 
 		std::string number_ = std::to_string(bookings[i]->roomNumber);
-		std::string date_ = std::to_string(bookings[i]->date);
-		std::string concat = "\nRoom Type : " + type_ + "\nRoom Number : " + number_ + "\nBooking Date : " + date_;
+		std::string date_ = std::to_string(bookings[i]->date); // get the variables for all the objects in the array and format them to strings
+		std::string concat = "\nRoom Type : " + type_ + "\nRoom Number : " + number_ + "\nBooking Date : " + date_; // Turn all the date into a print formatting
 		return concat;
 	}
 }
